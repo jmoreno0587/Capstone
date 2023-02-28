28afe1048b0548e4cb47464d1b52fe605c2eb2ff
JMoreno05 <57776551+JMoreno05@users.noreply.github.com>
2023-02-28 17:34:07 -0500

    env changes

--- Capstone/Capstone/models.py
+++ Capstone/Capstone/models.py
@@ -3,11 +3,14 @@ from flask import current_app as app
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    price = db.Column(db.Float)
-    def __init__(self, name, price):
-        self.name = name
-        self.price = price
+    userid = db.Column(db.Integer, primary_key=True)
+    userName = db.Column(db.String(255))
+    firstName = db.Column(db.String(255))
+    lastName = db.Column(db.String(255))
+    email = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    def __init__(self, userid, userName, firstName, lastName, email, password):
+        self.userName = userName
+        self.password = password
     def __repr__(self):
         return '<User %d>' % self.id
