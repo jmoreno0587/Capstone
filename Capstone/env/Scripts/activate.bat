13ede3a7114ae8b0b61a0e402883e38643c46ebc
Luke Tran <tranluket@gmail.com>
2023-03-02 15:40:49 -0500

    no real changes

--- Capstone/env/Scripts/activate.bat
+++ Capstone/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Repos\Capstone\Capstone\env
+set VIRTUAL_ENV=C:\Users\tranl\Source\Repos\Capstone\Capstone\env
 
 if not defined PROMPT set PROMPT=$P$G
 
